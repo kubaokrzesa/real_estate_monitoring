a02406a5fcf77610b5e36b55ddb2f97673c57458
Okrzesa-Jakub <okrzesa.jakub@bcg.com>
2023-11-04 12:05:51 +0100

    Created module for encoding lat and lon

---
+++ lat_lon_coding.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+from utils.setting_logger import Logger
+import re
+
+l = Logger(__name__)
+logger = l.get_logger()
+
+
+nominatim_timeout = 10
+nominatim_user_agent = "kubaokrzesa"
+
+
+class LatLonCoder:
+
+    def __init__(self, df):
+        self.df = df
+        self.df = self.df.dropna(subset=['adress'])
+        self.encoded_addresses = None
+
+    @staticmethod
+    def _locate_address(address):
+        logger.info(f"Finding latitude and longitude for: {address}")
+        ### Ul. in address confuses geolocator
+        address = address.replace("Ul. ", "").replace("ul. ", "").strip()
+        logger.info(f"Cleaned address: {address}")
+        geolocator = Nominatim(timeout=nominatim_timeout, user_agent=nominatim_user_agent)
+        location = geolocator.geocode(address)
+        if location is None:
+            address = ", ".join(address.split(', ')[1:])
+            logger.info(f"Making address wider: {address}")
+            location = geolocator.geocode(address)
+
+        if location:
+            logger.info(f"latitude: {location.latitude}, longitude: {location.longitude}")
+            res_dict = {'address': address, 'latitude': location.latitude, 'longitude': location.longitude}
+            return res_dict
+        else:
+            logger.info(f"Location not found")
+            res_dict = {'address': address, 'latitude': None, 'longitude': None}
+            return res_dict
+
+    def locate_addresses(self):
+        total_cases = self.df.shape[0]
+        cases_done = 0
+
+        res_list = []
+        try:
+            for _, row in self.df.iterrows():
+                try:
+                    res = self._locate_address(row['adress'])
+                    res['link'] = row['link']
+                except Exception as e:
+                    logger.info(f"Exception occured: {e}, Location not found")
+                    res = {'address': row['address'], 'latitude': None, 'longitude': None, 'link': row['link']}
+                finally:
+                    res_list.append(res)
+                    cases_done += 1
+                    logger.info(f"Processed {cases_done} / {total_cases} Cases")
+        except KeyboardInterrupt:
+            logger.info("User interrupted the process. Exiting...")
+        finally:
+            self.encoded_addresses = pd.DataFrame(res_list)
+
+    def save_encoded_lat_lon(self, path):
+        logger.info(f"Saving geocoding results to: {path}")
+        self.encoded_addresses.to_csv(path)
+
--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-import requests
-from scrapy import Selector
-from bs4 import BeautifulSoup
 import pandas as pd
+
 from scraping import Scraper
+from lat_lon_coding import LatLonCoder
+
+module_scraping = False
+module_lat_lon_coding = True
+
+initial_links_path = "initial_links.json"
+scraping_output_path = "offers.csv"
+address_matching_output_path = "geocoded_addresses.csv"
+
+max_page_num = 2 #243
 
 
-scraper = Scraper(max_page_num=10)
-scraper.collect_links_list()
-scraper.save_link_list("initial_links.json")
-scraper.extract_info_from_links()
-scraper.save_results("offers.csv")
+if module_scraping:
+    scraper = Scraper(max_page_num=max_page_num)
+    scraper.collect_links_list()
+    scraper.save_link_list()
+    scraper.extract_info_from_links(initial_links_path)
+    scraper.save_results(scraping_output_path)
+
+if module_lat_lon_coding:
+
+    df_adr = pd.read_csv("data/offers_full.csv", usecols=['link', 'adress'])# , nrows=100
+
+    lat_lon_coder = LatLonCoder(df=df_adr)
+    lat_lon_coder.locate_addresses()
+    lat_lon_coder.save_encoded_lat_lon(address_matching_output_path)
+
+
+
+
+
 
 
 
